6122b1327f392b1f913473d8930f4500f1dfc5b0
Roman_Bordiugov <rome-0@yandex.ru>
2019-11-08 11:58:16 +0300

    Tests cherry-picked

--- jdi-light-html-tests/src/main/java/io/github/com/entities/SupportMessage.java
+++ jdi-light-html-tests/src/main/java/io/github/com/entities/SupportMessage.java
@@ -3,6 +3,6 @@ package io.github.com.entities;
 import com.epam.jdi.tools.DataClass;
 
 public class SupportMessage extends DataClass<SupportMessage> {
-    public String supportInputEmail = "help-me.please@mail.org";
-    public String supportTextArea = "Nothing works! Nothing! Answer me, god damn it!";
+    public String supportEmail = "help-me.please@mail.org";
+    public String supportMessage = "Nothing works! Nothing! Answer me, god damn it!";
 }
--- jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/composite/BootstrapFormTests.java
+++ jdi-light-html-tests/src/test/java/io/github/epam/html/tests/elements/composite/BootstrapFormTests.java
@@ -1,7 +1,6 @@
 package io.github.epam.html.tests.elements.composite;
 
 import io.github.epam.TestsInit;
-import io.github.epam.test.data.SupportMessages;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -9,8 +8,12 @@ import static io.github.com.StaticSite.bsFormsPage;
 import static io.github.com.pages.BootstrapFormsPage.supportMessageForm;
 import static io.github.epam.html.tests.elements.BaseValidations.baseValidation;
 import static io.github.epam.html.tests.site.steps.States.shouldBeLoggedIn;
+import static org.hamcrest.Matchers.containsString;
 
 public class BootstrapFormTests implements TestsInit {
+
+    private final String logLineSubmit = "support-button-submit:button clicked";
+    private final String logLineClear = "support-button-clear:button clicked";
 
     @BeforeMethod
     public void before() {
@@ -19,12 +22,40 @@ public class BootstrapFormTests implements TestsInit {
     }
 
     @Test
-    public void submitForm() {
-        supportMessageForm.submit(SupportMessages.DEFAULT_MESSAGE);
+    public void submitButtonTest() {
+        supportMessageForm.supportButtonSubmit.click();
+        lastLogEntry.has().text(containsString(logLineSubmit));
+    }
+
+    @Test
+    public void clearButtonTest() {
+        supportMessageForm.supportButtonClear.click();
+        lastLogEntry.has().text(containsString(logLineClear));
+    }
+
+    @Test
+    public void submitFormTest() {
+        setDefaultValues();
+        supportMessageForm.submit(EXAMPLE_MESSAGE);
+        lastLogEntry.has().text(containsString(logLineSubmit));
+        supportMessageForm.check(EXAMPLE_MESSAGE);
+    }
+
+    @Test
+    public void clearFormTest() {
+        setDefaultValues();
+        supportMessageForm.clear(EXAMPLE_MESSAGE);
+        lastLogEntry.has().text(containsString(logLineClear));
+        supportMessageForm.check(TEMPLATE_MESSAGE);
     }
 
     @Test
     public void baseValidationTest() {
         baseValidation(supportMessageForm);
+    }
+
+    private void setDefaultValues() {
+        supportMessageForm.supportEmail.setValue(TEMPLATE_MESSAGE.supportEmail);
+        supportMessageForm.supportMessage.setValue(TEMPLATE_MESSAGE.supportMessage);
     }
 }
